1d0a2741119b361d17404fa489c9d076f128ff0c
Boluwatife Adewusi <bolusarz@gmail.com>
2021-10-04 09:14:04 +0100

    nav functions addded

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // api call will be made here
+
+$( document ).ready(function() {
+    $('.js-dashboard').on('click', function(){
+        $('.js-dashboard-page').removeClass('d-none');
+        $('.js-dashboard').removeClass('active');
+        $('.js-review-visits-page').addClass('d-none');
+        $('.js-review-visits').addClass('active');
+    });
+
+    $('.js-review-visits').on('click', function(){
+        $('.js-dashboard-page').addClass('d-none');
+        $('.js-dashboard').addClass('active');
+        $('.js-review-visits-page').removeClass('d-none');
+        $('.js-review-visits').removeClass('active');
+    });
+
+});
