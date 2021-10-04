5e77a3365ef0e61540222fef770f4c3dfe018c08
Boluwatife Adewusi <bolusarz@gmail.com>
2021-10-04 09:15:25 +0100

    bug fixes

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 $( document ).ready(function() {
     $('.js-dashboard').on('click', function(){
         $('.js-dashboard-page').removeClass('d-none');
-        $('.js-dashboard').removeClass('active');
+        $('.js-dashboard').addClass('active');
         $('.js-review-visits-page').addClass('d-none');
-        $('.js-review-visits').addClass('active');
+        $('.js-review-visits').removeClass('active');
     });
 
     $('.js-review-visits').on('click', function(){
         $('.js-dashboard-page').addClass('d-none');
-        $('.js-dashboard').addClass('active');
+        $('.js-dashboard').removeClass('active');
         $('.js-review-visits-page').removeClass('d-none');
-        $('.js-review-visits').removeClass('active');
+        $('.js-review-visits').addClass('active');
     });
 
 });
