c85c16b808e615193056ddbbcb267507e0eedd7e
Boluwatife Adewusi <bolusarz@gmail.com>
2021-10-07 13:36:41 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // api call will be made here
 
 $( document ).ready(function() {
-    $('.js-dashboard').on('click', function(){
+    let pages = ['js-dashboard'];
+
+
+    $('.js-dashboard, .nav-page').on('click', function(){
         $('.js-dashboard-page').removeClass('d-none');
         $('.js-dashboard').addClass('active');
         $('.js-review-visits-page').addClass('d-none');
         $('.js-review-visits').removeClass('active');
+        
     });
 
     $('.js-review-visits').on('click', function(){
@@ -13,6 +17,26 @@ $( document ).ready(function() {
         $('.js-dashboard').removeClass('active');
         $('.js-review-visits-page').removeClass('d-none');
         $('.js-review-visits').addClass('active');
+
+        getReviewVisits();
     });
 
+
+    let baseUrl = 'https://chioma.herokuapp.com/v1';
+
+    let data = {
+        
+    }
+
+    function getReviewVisits(){
+        $.ajax({
+            url: `${baseUrl}/visit`,
+            type: 'POST',
+            dataType: 'json', // added data type
+            success: function(res) {
+                console.log(res);
+                alert(res);
+            }
+        });
+    }
 });
